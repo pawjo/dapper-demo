f024105c7a2db5d95c1bfdfda0a59dce9e7869d7
Pawjo <pawima00@gmail.com>
2021-09-30 01:51:18 +0200

    Added getting last id in stored procedure

--- insert_stored_procedure.sql
+++ insert_stored_procedure.sql
@@ -3,12 +3,15 @@ DROP PROCEDURE dbo.sp_InsertNewAuthor
 GO
 
 CREATE PROCEDURE dbo.sp_InsertNewAuthor
-	@Id INT,
 	@FirstName NCHAR(20),
 	@LastName NCHAR(20), 
     @Nationality NCHAR(20)
 AS
 BEGIN
+
+DECLARE @Id INT;
+
+SET @Id = (SELECT MAX(Id) FROM Authors) + 1
 
 INSERT INTO Authors (Id, FirstName, LastName, Nationality)
 VALUES (@Id, @FirstName, @LastName, @Nationality)
